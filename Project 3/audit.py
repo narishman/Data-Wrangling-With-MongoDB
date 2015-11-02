eeaf66353fd63fbf4f4942282708f545d0adb7af
rmhyman <ransford.hyman@gmail.com>
2015-11-01 17:27:14 -0800

    Adding more notebooks and edits to auditing script
    Added new notebooks for Lesson 4/5 and made changes to my project
    notebook and audit to reflect progress being made on final project

--- Project 3/audit.py
+++ Project 3/audit.py
@@ -35,22 +35,43 @@ def audit_street_type(street_types, street_name):
         if street_type not in expected:
             street_types[street_type].add(street_name)
 
-
+def audit_key_values(key_vals,key_val):
+        key_vals.add(key_val)
+def audit_postcodes(post_codes,postcode):
+    post_codes.add(postcode)
+def audit_counties(counties,county):
+    counties.add(county)        
+def audit_tag_attribs(tag_names,tags):
+    #this function is to check whether the 'k' and 'v' are the only attributes of the tags
+    for tag_name in tags:
+        tag_names.add(tag_name)
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")
+def is_postcode(elem):
+    return (elem.attrib['k'] == "addr:postcode")
+def is_county(elem):
+    return (elem.attrib['k'] == "addr:county")
 
 
 def audit(osmfile):
     osm_file = open(osmfile, "r")
     street_types = defaultdict(set)
+    #tag_attribs = set()
+    key_vals = set()
+    post_codes = set()
+    counties = set()
     for event, elem in ET.iterparse(osm_file, events=("start",)):
-
         if elem.tag == "node" or elem.tag == "way":
             for tag in elem.iter("tag"):
+                #audit_tag_attribs(tag_attribs, tag.keys())
+                audit_key_values(key_vals, tag.attrib['k'])
                 if is_street_name(tag):
                     audit_street_type(street_types, tag.attrib['v'])
-
-    return street_types
+                elif is_postcode(tag):
+                    audit_postcodes(post_codes, tag.attrib['v'])
+                elif is_county(tag):
+                    audit_counties(counties, tag.attrib['v'])
+    return street_types,post_codes,key_vals,counties
 
 
 def update_name(name, mapping):
@@ -67,20 +88,14 @@ def update_name(name, mapping):
 
 
 def test():
-    st_types = audit(OSMFILE)
+    st_types,post_codes,key_vals,counties = audit(OSMFILE)
     #assert len(st_types) == 3
-    pprint.pprint(dict(st_types).keys())
-    #pprint.pprint(dict(st_types)
+    #pprint.pprint(dict(st_types).keys())
+    #pprint.pprint(dict(st_types))
+    pprint.pprint(post_codes)
+    pprint.pprint(counties)
+    #pprint.pprint(key_vals)
 
-    #for st_type, ways in st_types.iteritems():
-     #   for name in ways:
-            #better_name = update_name(name, mapping)
-            #print name, "=>", better_name
-            #if name == "West Lexington St.":
-             #   assert better_name == "West Lexington Street"
-            #if name == "Baldwin Rd.":
-             #   assert better_name == "Baldwin Road"
-
-
+    
 if __name__ == '__main__':
     test()
